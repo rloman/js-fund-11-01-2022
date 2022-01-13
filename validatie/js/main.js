6d9eb830122ad457438cd48bd885c8f559f16bfb
Raymond Loman <raymond@carpago.nl>
2022-01-13 15:29:28 +0100

    Add validatie

---
+++ validatie/js/main.js
@@ -0,0 +1,26 @@
+
+let re = /\d{4}\s?[A-Z]{2}/;
+
+function validateForm() {
+
+    let postcode = data.postcode.value;
+    let messages = "";
+
+    let resultValidation = re.test(postcode);
+    if(resultValidation == false) {
+        messages += "U dient een geldige postcode (4 cijfers en dan 2 letters) in te voeren!\n";
+    }
+
+    if(data.address.value.trim() == "") {
+        messages += "U dient een geldig huisadres in te voeren";
+    }
+
+
+    if(messages != "") {
+        alert(messages);
+
+        return false;
+    } else {
+        return true;
+    }
+}
